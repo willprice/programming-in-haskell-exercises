a5f8446f82bd31b63860fb0dec800c6e2e3325ed
Will Price <will.price94@gmail.com>
2021-01-02 19:03:06 +0000

    Add Luhn Algorithm and parity checking transmission

--- chapter-7.hs
+++ chapter-7.hs
@@ -49,27 +49,56 @@ unfold p h t x | p x = []
 bin2int binaryStr = foldr (\bit acc -> 2 * acc + bit) 0 binaryStr
 int2bin = unfold (== 0) (`mod` 2) (`div` 2)
 
+encode :: String -> [Bit]
+encode = concat . map (addParityBit . make8 . int2bin . ord)
+
+decode :: [Bit] -> String
+decode = map (chr . bin2int . checkParityBit) . (chopn 9)
+
 make8 :: [Bit] -> [Bit]
 make8 bits = take 8 (bits ++ repeat 0)
 
-encode :: String -> [Bit]
-encode = concat . map (make8 . int2bin . ord)
+chopn :: Int -> [Bit] -> [[Bit]]
+chopn n = unfold null (take n) (drop n)
 
+computeParity :: [Bit] -> Bit
+computeParity xs = (length $ filter (== 1) xs) `mod` 2
 
-chop8 :: [Bit] -> [[Bit]]
-chop8 = unfold null (take 8) (drop 8)
+addParityBit :: [Bit] -> [Bit]
+addParityBit xs = (computeParity xs) : xs
 
-decode :: [Bit] -> String
-decode = map (chr . bin2int) . chop8
+checkParityBit :: [Bit] -> [Bit]
+checkParityBit (parityBit:bits) 
+    | computeParity bits == parityBit = bits 
+    | otherwise = error "Parity bit check failed"
 
 transmit :: String -> String
 transmit = decode . channel . encode
 
 channel :: [Bit] -> [Bit]
 channel = id
+-- Exercise 8: drop parity bit to simulate comms failure
+--channel = tail
 
 map'' :: (a -> b) -> [a] -> [b]
 map'' fn = unfold null (fn . head) (drop 1)
 
 iterate' :: (a -> a) -> a -> [a]
 iterate' fn = unfold (const False) id fn
+
+altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
+altMap fnEven fnOdd xs = altMapEven xs
+    where 
+        altMapEven [] = []
+        altMapEven (x:xs) = fnEven x : altMapOdd xs
+        altMapOdd [] = []
+        altMapOdd (x:xs) = fnOdd x : altMapEven xs
+
+luhnDouble :: Int -> Int
+luhnDouble n 
+    | m > 9 = m - 9
+    | otherwise = m
+    where m = n * 2
+
+luhn :: [Int] -> Bool 
+luhn ns = sum (altMap id luhnDouble (reverse ns)) `mod` 10 == 0
